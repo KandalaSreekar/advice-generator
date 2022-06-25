46119e182b4d571d266c6ff6f5d541da20dac981
Sreekar <ksreekar407@gmail.com>
2022-06-25 17:27:17 +0530

    complete commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const adviceText = document.getElementById("advice-text")
+const adviceNumber = document.getElementById("advice-number")
+const refrestBtn = document.getElementById("get-data")
+
+refrestBtn.addEventListener("click", ()=> {
+    getAdvice();
+})
+
+window.onload = () => {
+    getAdvice();
+}
+
+function getAdvice (){
+fetch("https://api.adviceslip.com/advice").then(response=> {
+    return response.json();
+}).then(adviceData=>{
+    const Adviceobj = adviceData.slip
+    adviceText.textContent = Adviceobj.advice;
+    adviceNumber.textContent = Adviceobj.id;
+}).catch(error => {
+    console.log(error);
+})
+}
